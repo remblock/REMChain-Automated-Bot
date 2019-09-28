520cf9eed8ca681365ad669a2bf010ea2177e90f
SooSDExZ <54054688+SooSDExZ@users.noreply.github.com>
2019-09-28 10:38:52 +0930

    Update vote.sh

--- vote.sh
+++ vote.sh
@@ -46,7 +46,280 @@ fi
 #Create the directory if it does not exist
 if [ ! -d "$create_dir" ]
 then
+  mkdir "$create_dir"#!/bin/bash
+
+#If not root, exit the script
+if (($EUID!=0))
+then
+  echo "You must be root to run this script" 2>&1
+  exit 3
+fi
+
+#CONFIGURATION VARIABLES
+#directory to create
+create_dir="/root/info"
+#path to owner file
+owner_f="$create_dir/owneraccountname.txt"
+#path to wallet file
+wallet_f="$create_dir/walletpassword.txt"
+#path to block producers to voy
+bpaccountnames_f="$create_dir/bpaccountnames.txt"
+#path to telegram token
+tel_token_f="$create_dir/telegramtoken.txt"
+#path to telegram id
+tel_id_f="$create_dir/telegramid.txt"
+#path to automated vote file
+automated_vote_f="$create_dir/automatedvote.txt"
+#path to automated vote file
+automated_reward_f="$create_dir/automatedreward.txt"
+#path to automated vote file
+automated_vote_notifications_f="$create_dir/automatedvotenotifications.txt"
+#path to automated vote file
+automated_reward_notifications_f="$create_dir/automatedrewardnotifications.txt"
+#telegram message to send
+#check at the end of the script to change the messages
+#line to add to the crontab
+cron_line="1 0 * * * /root/vote.sh --cron"
+
+#Check if line is on cron already, if not add it
+if ! crontab -l | grep -v '^#' | grep vote.sh &>/dev/null
+then
+  echo "Crontab line added"
+  (crontab -l; echo "$cron_line" ) | crontab -
+fi
+
+#check if cron mode must be enabled
+cron=false
+if [[ "$1" == "--cron" ]]
+then
+  cron=true
+fi
+
+#Create the directory if it does not exist
+if [ ! -d "$create_dir" ]
+then
   mkdir "$create_dir"
+fi
+
+#unused function
+function pause(){
+      read -p "$*"
+}
+
+#get variable values from files or user
+if [ -f "$owner_f" ]
+then
+  owneraccountname=$(cat "$owner_f")
+else
+  if $cron
+  then
+    exit 1
+  fi
+  echo
+  read -p "YOUR ACCOUNT NAME: " -e owneraccountname
+  echo $owneraccountname > "$owner_f"
+  echo 
+fi
+
+if [ -f "$wallet_f" ]
+then
+  walletpassword=$(cat "$wallet_f")
+else
+  if $cron
+  then
+    exit 2
+  fi
+  read -p "YOUR WALLET PASSWORD: " -e walletpassword
+  echo $walletpassword > "$wallet_f"
+  echo 
+fi
+
+if [ -f "$automated_vote_f" ]
+then
+  automated_vote=$(cat "$automated_vote_f")
+else
+  if $cron
+  then
+    exit 2
+  fi
+  read -p "DO YOU WANT AUTOMATED VOTING (y/n): " -e automated_vote
+  automated_vote=$(echo $automated_vote | tr '[:upper:]' '[:lower:]')
+  echo $automated_vote > "$automated_vote_f"
+  echo 
+fi
+
+auto_vote=false
+auto_vote_noti=false
+if [[ "$automated_vote" == "y" || "$automated_vote" == "yes" ]] 
+then
+  auto_vote=true
+fi
+
+if $auto_vote
+then
+
+  if [ -f "$bpaccountnames_f" ]
+  then
+    bpaccountnames=$(cat "$bpaccountnames_f")
+  else
+    if $cron
+    then
+      exit 2
+    fi
+    read -p "BLOCK PRODUCERS TO VOTE FOR: " -e bpaccountnames
+    if [ -z "$bpaccountnames" ]
+    then
+      echo $owneraccountname > "$bpaccountnames_f"
+      bpaccountnames="$owneraccountname"
+    else
+      echo $bpaccountnames > "$bpaccountnames_f"
+    fi
+    echo 
+  fi
+
+  if [ -f "$automated_vote_notifications_f" ]
+  then
+    automated_vote_notifications=$(cat "$automated_vote_notifications_f")
+  else
+    if $cron
+    then
+      exit 2
+    fi
+    read -p "DO YOU WANT VOTE NOTIFICATIONS (y/n): " -e automated_vote_notifications
+    automated_vote_notifications=$(echo $automated_vote_notifications | tr '[:upper:]' '[:lower:]')
+    echo $automated_vote_notifications > "$automated_vote_notifications_f"
+    echo 
+  fi
+
+  if [[ "$automated_vote_notifications" == "y" || "$automated_vote_notifications" == "yes" ]] 
+  then
+    auto_vote_noti=true
+  fi
+
+fi
+
+if [ -f "$automated_reward_f" ]
+then
+  automated_reward=$(cat "$automated_reward_f")
+else
+  if $cron
+  then
+    exit 2
+  fi
+  read -p "DO YOU WANT AUTOMATED REWARDS (y/n): " -e automated_reward
+  automated_reward=$(echo $automated_reward | tr '[:upper:]' '[:lower:]')
+  echo $automated_reward > "$automated_reward_f"
+  echo 
+fi
+
+auto_reward=false
+auto_reward_noti=false
+if [[ "$automated_reward" == "y" || "$automated_reward" == "yes" ]] 
+then
+  auto_reward=true
+fi
+
+if $auto_reward
+then
+
+  if [ -f "$automated_reward_notifications_f" ]
+  then
+    automated_reward_notifications=$(cat "$automated_reward_notifications_f")
+  else
+    if $cron
+    then
+      exit 2
+    fi
+    read -p "DO YOU WANT REWARD NOTIFICATIONS (y/n): " -e automated_reward_notifications
+    automated_reward_notifications=$(echo $automated_reward_notifications | tr '[:upper:]' '[:lower:]')
+    echo $automated_reward_notifications > "$automated_reward_notifications_f"
+    echo 
+  fi
+
+  if [[ "$automated_reward_notifications" == "y" || "$automated_reward_notifications" == "yes" ]] 
+  then
+    auto_reward_noti=true
+  fi
+
+fi
+
+if $auto_vote_noti || $auto_reward_noti
+then
+
+  if [ -f "$tel_token_f" ]
+  then
+    tel_token=$(cat "$tel_token_f")
+  else
+    if $cron
+    then
+      exit 2
+    fi
+    read -p "COPY AND PASTE YOUR TELEGRAM TOKEN: " -e tel_token
+    echo $tel_token > "$tel_token_f"
+    echo 
+  fi
+
+  if [ -f "$tel_id_f" ]
+  then
+    tel_id=$(cat "$tel_id_f")
+  else
+    if $cron
+    then
+      exit 2
+    fi
+    read -p "COPY AND PASTE YOUR TELEGRAM CHAT ID: " -e tel_id
+    echo $tel_id > "$tel_id_f"
+    echo 
+  fi
+
+fi
+
+if $cron
+then
+  remcli wallet unlock --password $walletpassword &>/dev/null
+  if $auto_vote
+  then
+    remcli system voteproducer prods $owneraccountname $bpaccountnames -p $owneraccountname@vote &>/dev/null
+  fi
+  if $auto_reward
+  then
+    previous=$(remcli get currency balance rem.token $owneraccountname | awk '{print $1}')
+    remcli system claimrewards $owneraccountname -p $owneraccountname@claim &>/dev/null
+    after=$(remcli get currency balance rem.token $owneraccountname  | awk '{print $1}')
+    total_reward=$(echo "$after - $previous"|bc)
+  fi
+else
+  remcli wallet unlock --password $walletpassword
+  if $auto_vote
+  then
+    remcli system voteproducer prods $owneraccountname $bpaccountnames -p $owneraccountname@vote
+  fi
+  if $auto_reward
+  then
+    previous=$(remcli get currency balance rem.token $owneraccountname | awk '{print $1}')
+    remcli system claimrewards $owneraccountname -p $owneraccountname@claim
+    after=$(remcli get currency balance rem.token $owneraccountname  | awk '{print $1}')
+    total_reward=$(echo "$after - $previous"|bc)
+  fi
+fi
+
+#Telegram messages configuration
+tel_message_1="Your vote was casted for $bpaccountnames on $(date)"
+tel_message_2="$total_reward REM was received for $owneraccountname on $(date)"
+tel_message_3="$total_reward REM was received for $owneraccountname and your vote was casted for $bpaccountnames on $(date)"
+
+#Send notification to telegram
+if $auto_vote_noti && $auto_reward_noti
+then
+  curl -s -X POST https://api.telegram.org/bot$tel_token/sendMessage -d chat_id=$tel_id -d text="$tel_message_3" &>/dev/null
+elif $auto_vote_noti
+then
+  curl -s -X POST https://api.telegram.org/bot$tel_token/sendMessage -d chat_id=$tel_id -d text="$tel_message_1" &>/dev/null
+elif $auto_reward_noti
+then
+  curl -s -X POST https://api.telegram.org/bot$tel_token/sendMessage -d chat_id=$tel_id -d text="$tel_message_2" &>/dev/null
+fi
+
 fi
 
 #unused function
